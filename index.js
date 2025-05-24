30a258c0cb80f7607a5347372e1ad81583e45fc1
Asad <hellohello51981@gmail.com>
2025-05-24 11:16:21 +0500

    Adding javaScript - Project finished

--- index.js
+++ index.js
@@ -13,3 +13,55 @@ let player = {
     playerName: "Asad",
     playerBet: "145"
 }
+let playerEl = document.getElementById("playerEl");
+playerEl.textContent = player.playerName + ": $" + player.playerBet;
+
+function randomCard(){
+    let randomCard = Math.floor(Math.random()* 13) + 1;
+    if(randomCard ===1 ){
+        return 11;
+    }else if(randomCard >10){
+        return 10;
+    }else{
+    return randomCard;
+    }
+}
+
+start.addEventListener('click',startGame);
+function startGame(){
+    isAlive = true;
+    let firstCard = randomCard();
+    let secondCard = randomCard();
+    cards =[firstCard, secondCard];
+    sum = firstCard + secondCard;
+
+    renderGame();
+}
+function renderGame(){
+    sumEl.textContent = "Sum: " + sum;
+    cardsEl.textContent = "Card: ";
+
+for (let i = 0; i<cards.length; i++){
+    cardsEl.textContent+= cards[i] + " ";
+}
+    if (sum<=20){
+        message = "Do you want to draw a new Card? 🙂";
+    }else if(sum===21){
+        message = "Whoo...You got a blackjack! 😎";
+        hasBlackjack = true;
+    }else{
+        message = "Ops..You are out of the Game! 😭";
+        isAlive = false;
+    }
+    messageEl.textContent = message;
+}
+
+
+newBtn.addEventListener('click', function(){
+    if(isAlive && !hasBlackjack){
+        let newCard = randomCard();
+    sum += newCard;
+    cards.push(newCard);
+    renderGame();
+    }
+})
