cc02c82ae0b64c296c237d1bd9fd1687e6466e18
Asad <hellohello51981@gmail.com>
2025-05-24 11:13:55 +0500

    Adding javaScript - add new player

---
+++ index.js
@@ -0,0 +1,15 @@
+let card = []
+let sum = 0
+let hasBlackjack =false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message")
+let start =  document.getElementById("start")
+let sumEl = document.getElementById("sum")
+let cardsEl = document.getElementById("card")
+let newBtn = document.getElementById("new")
+
+let player = {
+    playerName: "Asad",
+    playerBet: "145"
+}
